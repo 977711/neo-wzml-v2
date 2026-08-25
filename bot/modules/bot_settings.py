4c610c8bae31d1c6f11c040881e326e1f4738891
977711 <izaz977711@gmail.com>
2026-08-25 11:32:46 +0500

    Update bot_settings.py

--- bot/modules/bot_settings.py
+++ bot/modules/bot_settings.py
@@ -133,8 +133,8 @@ async def get_buttons(key=None, edit_type=None, edit_mode=False, message=None):
     offset = start_dict.get(chat_id, 0) if chat_id else 0
     buttons = ButtonMaker()
     if key is None:
-        buttons.data_button("Aria2c Settings", "botset aria")
         buttons.data_button("Config Variables", "botset var")
+        buttons.data_button("Aria2c Settings", "botset aria")
         buttons.data_button("JDownloader Sync", "botset syncjd")
         buttons.data_button("Private Files", "botset private open")
         buttons.data_button("Qbit Settings", "botset qbit")
